6917df12e76606218070ff4640597146bb91ea11
Matheus Araújo de Souza <123311611+MatheusSouzaz@users.noreply.github.com>
2024-03-15 17:24:17 -0300

    Mudança nos botões finais.

--- src/js/botoes.js
+++ src/js/botoes.js
@@ -5,6 +5,26 @@ function mostrarMensagem(mensagem){
 function mostrarBotao(){
     let botaoSecreto = document.getElementsByClassName('botao-secreto')
     
-    botaoSecreto[0].classList.toggle('ativo')
+    botaoSecreto[0].classList.add('ativo')
     
 }
+
+document.querySelector('button[class = botao-secreto]').addEventListener('click', function(){
+    
+let botaoUltra = document.getElementsByClassName('botao-ultra-secreto')
+
+botaoUltra[0].classList.add('ativo')
+
+})
+
+document.querySelector('button[class = botao-ultra-secreto]').addEventListener('click', function(){
+    
+    let botaoSuper = document.getElementsByClassName('botao-super-secreto')
+    
+    botaoSuper[0].classList.add('ativo')
+
+    let botaoNormal = document.getElementsByClassName('botao-normal')
+    
+    botaoNormal[0].classList.add('ativo')
+    
+    })
